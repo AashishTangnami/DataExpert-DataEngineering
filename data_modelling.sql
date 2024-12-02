9d54695deda240c13c3644c7fe03fbffccff068d
Aashish Tangnami <42236161+AashishTangnami@users.noreply.github.com>
2024-12-02 14:34:51 -0500

    analytics through struct and array

--- data_modelling.sql
+++ data_modelling.sql
@@ -1,109 +1,25 @@
  -- Lecture Lab: 1
-
-SELECT * FROM player_seasons;
-
-
---- Creating a Struct
-CREATE TYPE season_stats AS (
-	season  INTEGER,
-	gp INTEGER,
-	pts REAL,
-	reb REAL,
-	ast REAL
-);
-
-
--- Creating a table for players with a data type struct for season_stats
-CREATE TABLE players (
-	player_name TEXT,
-	height TEXT,
-	colelge TEXT,
-	country TEXT,
-	draft_year TEXT, 
-	draft_round TEXT,
-	draft_number TEXT,
-	season_stats season_stats[],
-	current_season INTEGER,
-	PRIMARY KEY (player_name, current_season)
-);
-
---- 
-ALTER TABLE players
-RENAME COLUMN colelge TO college;
-
-SELECT min(season) from player_seasons;
-
-
---- Inserting data into the players table.
-INSERT INTO players
-WITH yesterday AS (
-	SELECT * FROM players
-	WHERE current_season = 2000
-),
- today AS ( SELECT * FROM player_seasons
- WHERE season = 2001
- )
- SELECT 
-	 COALESCE(t.player_name , y.player_name ) AS player_name,
-	 COALESCE(t.height , y.height ) AS height,
-	 COALESCE(t.college , y.college ) AS college,
-	 COALESCE(t.country , y.country ) AS country,
-	 COALESCE(t.draft_year , y.draft_year ) AS draft_year,
-	 COALESCE(t.draft_round , y.draft_round ) AS draft_round,
-	 COALESCE(t.draft_number, y.draft_number ) AS draft_number,
-	 CASE WHEN y.season_stats IS NULL
-	 	THEN ARRAY[ROW(
-			t.season,
-			t.gp,
-			t.pts,
-			t.reb,
-			t.ast
-		)::season_stats]
- 	 WHEN t.season IS NOT NULL THEN y.season_stats || ARRAY[ROW(
-			t.season,
-			t.gp,
-			t.pts,
-			t.reb,
-			t.ast
-		)::season_stats]
-	 ELSE y.season_stats
- 	 END AS season_stats,
-	  COALESCE (t.season, y.current_season + 1) as current_season
- FROM 
- today t FULL OUTER JOIN yesterday y 
- ON t.player_name = y.player_name
-
--------------- Querying the players table ----------------------
- WITH
-	UNNESTED AS (
-		SELECT
-			PLAYER_NAME,
-			UNNEST(SEASON_STATS)::SEASON_STATS AS SEASON_STATS
-		FROM
-			PLAYERS
-		WHERE
-			CURRENT_SEASON = 1998
-			AND PLAYER_NAME = 'Micheal Jordan'
-	)
 SELECT
-	PLAYER_NAME,
-	(SEASON_STATS::SEASON_STATS).*
+	*
 FROM
-	UNNESTED
-
------- Dropping the table players ---
-
-DROP TABLE players;
+	PLAYER_SEASONS;
 
 
---- Creating an enum type called scoring_class -----
-CREATE TYPE SCORING_CLASS AS ENUM('star', 'good', 'average', 'bad');
 
---- Creating a table for players with a data type struct for added scoring_class and year_since_last_season
+CREATE TYPE SEASON_STATS AS (
+	SEASON INTEGER,
+	GP INTEGER,
+	PTS REAL,
+	REB REAL,
+	AST REAL
+);
+
+CREATE TYPE SCORING_CLASS AS ENUM('star', 'good', 'average', 'bad')
+
 CREATE TABLE PLAYERS (
 	PLAYER_NAME TEXT,
 	HEIGHT TEXT,
-	COLLEGE TEXT,
+	COLELGE TEXT,
 	COUNTRY TEXT,
 	DRAFT_YEAR TEXT,
 	DRAFT_ROUND TEXT,
@@ -116,8 +32,8 @@ CREATE TABLE PLAYERS (
 );
 
 
----------- Inserting data into the players table ------------
 
+--- Inserting data into the players table.
 INSERT INTO
 	PLAYERS
 WITH
@@ -127,7 +43,7 @@ WITH
 		FROM
 			PLAYERS
 		WHERE
-			CURRENT_SEASON = 1995
+			CURRENT_SEASON = 2002
 	),
 	TODAY AS (
 		SELECT
@@ -135,7 +51,7 @@ WITH
 		FROM
 			PLAYER_SEASONS
 		WHERE
-			SEASON = 1996
+			SEASON = 2002
 	)
 SELECT
 	COALESCE(T.PLAYER_NAME, Y.PLAYER_NAME) AS PLAYER_NAME,
@@ -164,12 +80,61 @@ SELECT
 		ELSE Y.SCORING_CLASS
 	END,
 	CASE
-		WHEN T.SEASON IS NOT NULL THEN 1
-	END,
-	CASE
 		WHEN T.SEASON IS NOT NULL THEN 0
-		ELSE COALESCE(Y.YEARS_SINCE_LAST_SEASON, 0) + 1
-	END AS YEARS_SINCE_LAST_SEASON COALESCE(T.SEASON, Y.CURRENT_SEASON + 1) AS CURRENT_SEASON
+		ELSE COALESCE(Y.YEAR_SINCE_LAST_SEASON, 0) + 1
+	END AS YEAR_SINCE_LAST_SEASON,
+	
+	COALESCE(T.SEASON, Y.CURRENT_SEASON + 1) AS CURRENT_SEASON
 FROM
 	TODAY T
 	FULL OUTER JOIN YESTERDAY Y ON T.PLAYER_NAME = Y.PLAYER_NAME
+
+
+
+
+
+WITH
+	UNNESTED AS (
+		SELECT
+			PLAYER_NAME,
+			UNNEST(SEASON_STATS)::SEASON_STATS AS SEASON_STATS --- season component un nested
+		FROM
+			PLAYERS
+		WHERE
+			CURRENT_SEASON = 1998
+			AND PLAYER_NAME = 'Micheal Jordan'
+	)
+SELECT
+	PLAYER_NAME,
+	(SEASON_STATS::SEASON_STATS).*
+FROM
+	UNNESTED
+
+
+SELECT * FROM PLAYERS
+WHERE CURRENT_SEASON = 2000
+AND PLAYER_NAME = 'Michael Jordan';
+
+
+SELECT
+	PLAYER_NAME,
+	SEASON_STATS[1] AS FIRST_SEASON,
+	SEASON_STATS[CARDINALITY(SEASON_STATS)] AS LATEST_SEASON
+FROM
+	PLAYERS
+WHERE
+	CURRENT_SEASON = 2001;
+
+--- ANALYTICS ---
+SELECT
+	PLAYER_NAME,
+	(
+		SEASON_STATS[CARDINALITY(SEASON_STATS)]::SEASON_STATS
+	).PTS / CASE
+		WHEN (SEASON_STATS[1]::SEASON_STATS).PTS = 0 THEN 1
+		ELSE (SEASON_STATS[1]::SEASON_STATS).PTS
+	END AS AVG_STATS
+FROM
+	PLAYERS
+WHERE
+	CURRENT_SEASON = 2001 AND SCORING_CLASS = 'star';
